eda0f7e1bd739dc4c66736cd035dd9454adeb862
Ivy <ivyadisa7@gmail.com>
2025-08-14 22:50:57 +0300

    MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -7,11 +7,11 @@ import mysql.connector
 from mysql.connector import Error
 
 try:
-    # Connect to MySQL server (update user and password if needed)
+    # Connect to MySQL server (update credentials)
     connection = mysql.connector.connect(
         host='localhost',
-        user='root',       # Change to your MySQL username
-        password='Adisa@2718'    # Change to your MySQL password
+        user='root',        # change to your MySQL username
+        password='root'     # change to your MySQL password
     )
 
     if connection.is_connected():
@@ -20,13 +20,10 @@ try:
         print("Database 'alx_book_store' created successfully!")
 
 except Error as e:
-    print(f"Error: {e}")
+    print(f"Error while connecting to MySQL: {e}")
 
 finally:
-    # Close cursor and connection
-    try:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-    except NameError:
-        pass
+    if 'cursor' in locals() and cursor:
+        cursor.close()
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
